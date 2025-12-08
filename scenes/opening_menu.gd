5392323c370be724e7d5f3f1b2f066028afea129
TroyAWilson <wilsontr@sonoma.edu>
2025-12-08 11:03:52 -0800

    new music + art

--- scenes/opening_menu.gd
+++ scenes/opening_menu.gd
@@ -6,6 +6,5 @@ func _on_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_leaderboard_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/leaderboard_screen.tscn")
